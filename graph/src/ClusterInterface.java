3807966f750655106eb9ca46452593dfc15273e5
Kathrine Smoliansky <kate.smo@gmail.com>
2019-01-28 20:55:22 +0200

    created cluster and add src file which containes Sp_Cons class

---
+++ graph/src/ClusterInterface.java
@@ -0,0 +1,27 @@
+package src;
+
+import sun.security.provider.certpath.Vertex;
+
+import java.util.Collection;
+import java.util.Set;
+
+public interface ClusterInterface {
+
+    Vertex getVertex(int vertexId);
+
+    boolean addVertex(Vertex var1);
+    boolean addVertices(Set<Vertex> add);
+
+    boolean containsVertex(Vertex var1);
+
+    boolean removeAllVertixes(Collection<? extends Vertex> var1);
+
+    boolean removeVertex(Vertex var1);
+
+    Set<Vertex> getVertices();
+
+    String toString();
+
+    int numOfVertices();
+
+}
